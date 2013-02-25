6f7de06c99c7718c1d156f4992597486cf2b502f
David Hewitson <dwhewitson@gmail.com>
2013-02-25 22:24:40 +0000

    started a total rewrite in coffeescript

--- client/js/ui.js
+++
@@ -1,9 +0,0 @@
-Meteor.startup(function() {
-	// temporary interface
-	$("#ui-save").click(function() {
-		alert(JSON.stringify(loadedMap));
-	});
-	$("#ui-load").click(function() {
-		alert("Doesn't work yet. Sorry.");
-	});
-});
